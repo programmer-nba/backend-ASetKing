365db27a1532b7211a49f393a113d9844db7e2fa
กายสุดหล่อ <warunyoo084@gmail.com>
2024-02-13 10:09:03 +0700

    13/02/2024

--- model/product/history.create.product.model.js
+++ model/product/history.create.product.model.js
@@ -2,31 +2,38 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const ProductCreateHisSchema = new mongoose.Schema({
-  number: {type: Number, required: true},
-  status: {type: String, required: false, default: ""},
-  category_main: {type: String, require: true},
-  category_second: {type: String, require: false, default: ""},
-  model: {type: String, require: true},
-  pricture: {type: String, require: false, default: ""},
-  hl: {type: String, require: true},
-  description: {type: String, require: true},
+  number: { type: Number, required: true },
+  status: { type: String, required: false, default: "" },
+  category_main: { type: String, require: true },
+  category_second: { type: String, require: false, default: "" },
+  model: { type: String, require: true },
+  pricture: { type: String, require: false, default: "" },
+  hl: { type: String, require: true },
+  description: { type: String, require: true },
   price: {
     type: {
-      one: {type: Number, required: true},
-      two: {type: Number, required: true},
-      tree: {type: Number, required: true},
-      four: {type: Number, required: true},
-      five: {type: Number, required: true},
-      six: {type: Number, required: true},
+      one: { type: Number, required: true },
+      two: { type: Number, required: true },
+      tree: { type: Number, required: true },
+      four: { type: Number, required: true },
+      five: { type: Number, required: true },
+      six: { type: Number, required: true },
     },
   },
-  note: {type: String, require: false, default: ""},
-  lnsure: {type: String, require: false, default: ""}, //ประกัน
-  update: {type: Array, required: false, default: []},
-  link_spec: {type: String, require: false, default: ""},
-  link_document: {type: String, require: false, default: ""},
-  link_img: {type: String, require: false, default: ""},
+  note: { type: String, require: false, default: "" },
+  lnsure: { type: String, require: false, default: "" }, //ประกัน
+  update: { type: Array, required: false, default: [] },
+  link_spec: { type: String, require: false, default: "" },
+  link_document: { type: String, require: false, default: "" },
+  link_img: { type: String, require: false, default: "" },
+  update: [{
+    name:{ type: String, require: false, default: "" },
+    timestamps: { type: Date, required: false, default: Date.now() },
+  }],
 });
 
-const ProductsHistory = mongoose.model("ProductsHistory", ProductCreateHisSchema);
-module.exports = {ProductsHistory};
+const ProductsHistory = mongoose.model(
+  "ProductsHistory",
+  ProductCreateHisSchema
+);
+module.exports = { ProductsHistory };
